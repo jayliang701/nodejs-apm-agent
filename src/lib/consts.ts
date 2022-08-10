79b1c9612b19c6f9f8cda818e6b53d844e1d8918
Jay Liang <156070304@qq.com>
2022-08-10 15:00:56 +0800

    实现RPC推送metric数据

--- src/lib/consts.ts
+++ src/lib/consts.ts
@@ -1,6 +1,23 @@
 import { homedir } from "os";
 import path from "path";
+import { AgentConfig } from "./types";
 
 export const PROCESS_EXIT_EVENTS: string[] = [`exit`, `SIGINT`, `SIGUSR1`, `SIGUSR2`, `uncaughtException`, `SIGTERM`];
 
 export const PERSIST_FILE_PATH: string = path.resolve(homedir(), '.apm-agent.json');
+
+export const DEFAULT_AGENT_CONFIG: AgentConfig = {
+    service: process.env.APM_ENDPOINT_SERVICE,
+    serviceInstance: process.env.APM_ENDPOINT_SERVICE_INSTANCE,
+    serverAddress: process.env.APM_SERVER_ADDRESS || '127.0.0.1:12700',
+    collect: {
+        log: {
+            enabled: false,
+        },
+        metric: {
+            enabled: false,
+            duration: 5,
+            maxLen: 600,
+        }
+    }
+};
---
+++ src/lib/utils/index.ts
@@ -0,0 +1,15 @@
+export function defaultSet<S extends object, T extends object>(defaultObj: S, target: T): T {
+    for (let key in defaultObj) {
+        const defaultVal = defaultObj[key];
+        const kt = key as any;
+        if (typeof defaultVal === 'object' && !(defaultVal instanceof RegExp || defaultVal instanceof Date)) {
+            target[kt] = defaultSet(defaultVal as any, target[kt] || {});
+        } else {
+            const val = target[kt];
+            if (!target.hasOwnProperty(kt) || val === undefined || val === null) {
+                target[kt] = defaultObj[key];
+            }
+        }
+    }
+    return target;
+}
